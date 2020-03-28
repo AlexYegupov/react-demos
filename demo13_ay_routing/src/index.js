eba1571dc32d87a3cfaf89201a61016e95637b73
Alex_Y <yegupov@gmail.com>
2020-03-28 20:07:14 +0200

    optimized

--- demo13_ay_routing/src/index.js
+++ demo13_ay_routing/src/index.js
@@ -1,16 +1,1 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './app';
-//
-// // ReactDOM.render(<App items={window.APP_PROPS.items} url={window.URL} />, document.getElementById('content'));
-//
-// const wrapper = document.getElementById("content");
-//
-// if (wrapper) {
-//   ReactDOM.render(
-//     <App items={window.APP_PROPS.items} url={window.URL} />,
-//     wrapper
-//   )
-// }
-
-import Form from "./js/components/Form";
+import Form from "./Form";
